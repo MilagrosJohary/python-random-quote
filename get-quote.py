4378823bd04bf63b972410ff749952b5ce014ea9
Milagros <johary.padilla2@unmsm.edu.pe>
2020-03-24 19:11:55 -0500

    Completed extra exercises

--- get-quote.py
+++ get-quote.py
@@ -3,13 +3,42 @@ import random
 def primary():
  #print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
+  ############## Eliminar espacios #########################
+  
+  f = open("quotes.txt",'r',encoding='utf-8')
   quotes = f.readlines()
   f.close()
-  
-  last=13
-  rnd=random.randint(0, last)
-  print(quotes[rnd])
+  qty = int(input("Ingresar cantidad de frases: "))
+  for i in range(0,qty):
+    rnd=random.randint(0, len(quotes)-1)
+    rnd_quote=quotes[rnd]
+    print(rnd_quote,end='')
+
+
+  ############### Cantidad de frases aleatorias ############
+  #f = open("quotes.txt",'r',encoding='utf-8')
+  #quotes = f.readlines()
+  #f.close()
+  #qty = int(input("Ingresar cantidad de frases: "))
+  #for i in range(0,qty):
+  #  rnd=random.randint(0, len(quotes)-1)
+  #  rnd_quote=quotes[rnd]
+  #  print(rnd_quote)
+
+  ############## Agregar frases ############################
+
+  #f = open("quotes.txt",'a+')
+  #new_quote = input("Ingresar frase: ")
+  #f.write(new_quote + '\n')
+  #f.close()
+   
+  ############# Leer frases random ########################
+  #f = open("quotes.txt")
+  #quotes = f.readlines()
+  #f.close()
+  #last=13
+  #rnd=random.randint(0, last)
+  #print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
